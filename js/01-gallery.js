25b7463819bd4f2ebf4261460567df5f3dd97c83
Dmytro Muzyka <mmitya889@gmail.com>
2022-10-21 00:35:44 +0300

    refactoring to OOP

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,58 +1,72 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryRef = document.querySelector(".gallery");
-
-createGalleryMarkup(galleryRef, galleryItems);
-
-galleryRef.addEventListener("click", onGalleryImageClick);
-
-function onGalleryImageClick(e) {
-  e.preventDefault();
-  if (!e.target.classList.contains("gallery__image")) {
-    return;
+class Gallery {
+  constructor(galleryRef, galleryItems) {
+    this.galleryRef = galleryRef;
+    this.galleryItems = galleryItems;
   }
 
-  const imgSrc = e.target.dataset.source;
-  initLightbox(imgSrc);
+  init() {
+    this.createGalleryMarkup(this.galleryRef, this.galleryItems);
+    this.addListeners(this.galleryRef);
+  }
+
+  addListeners(elem) {
+    elem.addEventListener("click", this.onGalleryImageClick.bind(this));
+  }
+
+  onGalleryImageClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("gallery__image")) {
+      return;
+    }
+
+    const imgSrc = e.target.dataset.source;
+    this.initLightbox(imgSrc);
+  }
+
+  createGalleryMarkup(galleryContainerRef, galleryItems) {
+    const galleryItemsMarkup = galleryItems
+      .map((item) => this.createGalleryItem(item))
+      .join("");
+
+    galleryContainerRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+  }
+
+  createGalleryItem(galleryItem) {
+    const galleryLink = this.createGalleryLink(galleryItem);
+    return `
+      <div class="gallery__item">
+        ${galleryLink}
+      </div>
+    `;
+  }
+
+  createGalleryLink({ preview, original, description }) {
+    return `
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    `;
+  }
+
+  createLightboxImg(srcLink) {
+    return `<img src="${srcLink}" width="800" height="600">`;
+  }
+
+  initLightbox(srcLink) {
+    const lightboxImg = this.createLightboxImg(srcLink);
+    const instance = basicLightbox.create(lightboxImg);
+    instance.show();
+  }
 }
 
-function createGalleryMarkup(galleryContainerRef, galleryItems) {
-  const galleryItemsMarkup = galleryItems
-    .map((item) => createGalleryItem(item))
-    .join("");
+const galleryRef = document.querySelector(".gallery");
 
-  galleryContainerRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
-}
-
-function createGalleryItem(galleryItem) {
-  const galleryLink = createGalleryLink(galleryItem);
-  return `
-  <div class="gallery__item">
-    ${galleryLink}
-  </div>
-  `;
-}
-
-function createGalleryLink({ preview, original, description }) {
-  return `
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-  `;
-}
-
-function createLightboxImg(srcLink) {
-  return `<img src="${srcLink}" width="800" height="600">`;
-}
-
-function initLightbox(srcLink) {
-  const lightboxImg = createLightboxImg(srcLink);
-  const instance = basicLightbox.create(lightboxImg);
-  instance.show();
-}
+new Gallery(galleryRef, galleryItems).init();
