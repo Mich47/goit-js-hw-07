6bf7960efe771f28ed61bc52a71cc8c1ef25df00
Dmytro Muzyka <mmitya889@gmail.com>
2022-10-21 00:16:30 +0300

    add task-1 without Esc close

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,58 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-{
-  /* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
+const galleryRef = document.querySelector(".gallery");
+
+createGalleryMarkup(galleryRef, galleryItems);
+
+galleryRef.addEventListener("click", onGalleryImageClick);
+
+function onGalleryImageClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgSrc = e.target.dataset.source;
+  initLightbox(imgSrc);
 }
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryContainerRef, galleryItems) {
+  const galleryItemsMarkup = galleryItems
+    .map((item) => createGalleryItem(item))
+    .join("");
+
+  galleryContainerRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+}
+
+function createGalleryItem(galleryItem) {
+  const galleryLink = createGalleryLink(galleryItem);
+  return `
+  <div class="gallery__item">
+    ${galleryLink}
+  </div>
+  `;
+}
+
+function createGalleryLink({ preview, original, description }) {
+  return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  `;
+}
+
+function createLightboxImg(srcLink) {
+  return `<img src="${srcLink}" width="800" height="600">`;
+}
+
+function initLightbox(srcLink) {
+  const lightboxImg = createLightboxImg(srcLink);
+  const instance = basicLightbox.create(lightboxImg);
+  instance.show();
+}
